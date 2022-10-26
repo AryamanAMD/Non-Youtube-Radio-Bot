d86938d7f4aa2ab78f8e0cc9b86296764a721645
AryamanAMD <aryamanatamd@gmail.com>
2022-10-26 17:10:10 +0530

    Create ffprobe.exe

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+
 import discord
 from discord import FFmpegPCMAudio
 from discord.ext.commands import Bot
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 #TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = Bot(command_prefix='!',intents=discord.Intents.all())
+#PREFIX = os.getenv('DISCORD_PREFIX')
+intents=discord.Intents.all()
+client = Bot(command_prefix='>>',intents=intents)
 
 
 @client.event
@@ -16,20 +17,19 @@ async def on_ready():
 
 
 @client.command(aliases=['p', 'pla'])
-async def play(ctx, url: str = 'https://streams.ilovemusic.de/iloveradio109.mp3'):  #http://stream.radioparadise.com/rock-128
+async def play(ctx, url: str = 'http://stream.radioparadise.com/rock-128'):
     channel = ctx.message.author.voice.channel
     global player
     try:
         player = await channel.connect()
     except:
         pass
-    player.play(FFmpegPCMAudio(executable='ffmpeg/bin/ffmpeg.exe',source='http://stream.radioparadise.com/rock-128'))
+    player.play(FFmpegPCMAudio('http://stream.radioparadise.com/rock-128',executable='ffmpeg/bin/ffmpeg.exe'))
 
 
 @client.command(aliases=['s', 'sto'])
 async def stop(ctx):
     player.stop()
-  
 
 
-client.run('OTg1MTc2OTg1OTUxMTY2NDk1.Gk5k6K._Oaaj8DmXVp8hfUc0EP-YfLIQvLAU1ybsExhN0')
+client.run('OTg1MTc2OTg1OTUxMTY2NDk1.Gg1ao9.Y2LSW8zOcRCwUNFurway8okgj3vPA-cM6DFvvo')
