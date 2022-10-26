bdc71bf94be7eb8dfb1ff8452f2bee6c77138ba3
AryamanAMD <aryamanatamd@gmail.com>
2022-10-26 16:35:09 +0530

    added ffmpeg

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import os
-
+import discord
 from discord import FFmpegPCMAudio
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-PREFIX = os.getenv('DISCORD_PREFIX')
+#TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = Bot(command_prefix=list(PREFIX))
+client = Bot(command_prefix='!',intents=discord.Intents.all())
 
 
 @client.event
@@ -24,17 +23,13 @@ async def play(ctx, url: str = 'https://streams.ilovemusic.de/iloveradio109.mp3'
         player = await channel.connect()
     except:
         pass
-    player.play(FFmpegPCMAudio('http://stream.radioparadise.com/rock-128'))
+    player.play(FFmpegPCMAudio(executable='ffmpeg/bin/ffmpeg.exe',source='http://stream.radioparadise.com/rock-128'))
 
 
 @client.command(aliases=['s', 'sto'])
 async def stop(ctx):
     player.stop()
-
-@client.command(aliases=['c', 'change'])
-async def stop(ctx,arg):
-    ctx.send("This is for Non-Youtube Streams only.")
-    str=arg   
+  
 
 
-client.run(TOKEN)
+client.run('OTg1MTc2OTg1OTUxMTY2NDk1.Gk5k6K._Oaaj8DmXVp8hfUc0EP-YfLIQvLAU1ybsExhN0')
